527f1b12b3444f2310b6512fd7f3ed1560fb7772
Fajriansyah Pratama <fajriansyahpratama2103@gmail.com>
2026-06-09 18:21:41 +0700

    Add user session check before deleting expense

--- Mapan/hapus_pengeluaran.php
+++ Mapan/hapus_pengeluaran.php
@@ -1,14 +1,20 @@
 <?php
-
 session_start();
 include "koneksi.php";
 
-$id = $_GET['id'];
+if (!isset($_SESSION['username'])) {
+    header("Location: login.php");
+    exit;
+}
+
+$id_user = $_SESSION['id_user'];
+$id = isset($_GET['id']) ? (int) $_GET['id'] : 0;
 
 mysqli_query(
     $koneksi,
-    "DELETE FROM pengeluaran
-    WHERE id_pengeluaran='$id'"
+    "DELETE FROM pengeluaran WHERE id_pengeluaran='$id' AND id_user='$id_user'"
 );
 
-header("Location: pengeluaran.php");
+header("Location: Pengeluaran.php");
+exit;
+?>
